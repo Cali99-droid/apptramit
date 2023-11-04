35e90ec5add29070dd1d1c1942f79c353f4d916d
carlos <orellano428@gmail.com>
2023-11-04 10:34:43 -0500

    mensaje

--- frontend/src/views/Registro.jsx
+++ frontend/src/views/Registro.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Alert, AlertTitle, Box, Container, Typography } from "@mui/material";
 
 import SolicitudRequestForm from "../components/SolicitudRequestForm";
 
@@ -10,6 +10,11 @@ export default function Registro() {
        <Typography variant="h4" align="center" gutterBottom>
           Solicitud de trámite
         </Typography>
+        <Alert severity="info">
+        <AlertTitle>Importante</AlertTitle>
+        Al finalizar el envio el sistema le asignará un codigo para que pueda dar seguimiento a su documento <strong>copie</strong> el codigo y guárdelo en un lugar seguro
+      </Alert>
+      <br />
         <SolicitudRequestForm />
        </Box>
       </Container>
